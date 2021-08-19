2388be3fc328ca17994cf32a71106884b44603e5
Queeniva <ngulefacbenazea@gmail.com>
2021-08-19 20:11:31 +0100

    finished the navbar and also installed the dependencies fro the movie trailer

--- src/Nav.js
+++ src/Nav.js
@@ -1,8 +1,22 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 
 function Nav() {
+    const [show, handleShow] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 100) {
+                handleShow(true)
+            } else {
+                handleShow(false)
+            }
+        });
+        return () => {
+            window.removeEventListener("scroll");
+        };
+    }, [])
     return (
-        <div className ="nav">
+        <div className={`nav ${show && "nav__black"}`}>
             <img
                 className="nav__logo"
                 src="https://www.freepnglogos.com/uploads/netflix-logo-0.png"
