68c6f33876e9c36ec21b7ff021bfee31bdba3e35
Queeniva <ngulefacbenazea@gmail.com>
2022-03-28 04:48:29 +0100

    popps now work

--- src/Row.js
+++ src/Row.js
@@ -1,12 +1,28 @@
 import React, { useEffect, useState } from 'react'
 import axios from './axios';
-// import YouTube from 'react-youtube';
-
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
 const base_url = "https://image.tmdb.org/t/p/original";
 
 function Row({ title, fetchURL , isLargeRow}) {
 
     const [movies, setmovies] = useState([]);
+    const [trailerUrl, setTrailerUrl] = useState("");
+
+    const handleClick = (movie) =>{
+        if(trailerUrl){
+            setTrailerUrl('');
+        }else{
+            movieTrailer(movie?.title || "")
+            .then((url) =>{
+                const urlParams = new URLSearchParams(new URL(url).search);
+                const videoId = urlParams.get('v');
+                setTrailerUrl(videoId);
+                console.log('love', trailerUrl);
+               
+            }).catch((error) => console.log(error));
+        }
+    }
 
     useEffect(() => {
         async function fetchData() {
@@ -30,10 +46,10 @@ function Row({ title, fetchURL , isLargeRow}) {
             <h2>{title}</h2>
             <div className="row__posters">
                 {movies.map(movie => (
-                    <img key={movie.id} className={`row__poster ${isLargeRow && "row__posterLarge"}`} src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.original_title} />
+                    <img key={movie.id} onClick={()=>handleClick(movie)} className={`row__poster ${isLargeRow && "row__posterLarge"}`} src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.original_title} />
                 ))}
             </div>
-            {/* <YouTube videoId={trailerUrl} opts={opts} /> */}
+            {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
         </div>
     )
 }
